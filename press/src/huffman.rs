b6da315418423f0da6014422d7c025b5ed0fe602
Brice Fernandes <brice@fractallambda.com>
2024-01-25 18:32:48 +0000

    Format

--- press/src/huffman.rs
+++ press/src/huffman.rs
@@ -1,25 +1,23 @@
-
-use core::panic;
-use std::collections::HashMap;
-use std::collections::BinaryHeap;
-use core::hash::Hash;
 use bitvec::prelude::*;
 use bitvec::vec::BitVec;
-
+use core::hash::Hash;
+use core::panic;
+use std::collections::BinaryHeap;
+use std::collections::HashMap;
 
 #[allow(dead_code)]
 #[derive(Debug)]
 pub enum HuffmanError {
-    String(&'static str)
+    String(&'static str),
 }
 
-#[derive(PartialEq,Eq,Ord,PartialOrd,Hash,Clone,Copy)]
+#[derive(PartialEq, Eq, Ord, PartialOrd, Hash, Clone, Copy)]
 pub enum Symbol {
     Char(u8),
-    EOT
+    EOT,
 }
 
-impl std::fmt::Debug for Symbol{
+impl std::fmt::Debug for Symbol {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Char(c) => write!(f, "'{}'", *c as char),
@@ -33,11 +31,11 @@ pub enum HuffmanTree {
         freq: u64,
         symbol: Symbol,
     },
-    Node{
+    Node {
         freq: u64,
         left: Box<HuffmanTree>,
-        right: Box<HuffmanTree>
-    }
+        right: Box<HuffmanTree>,
+    },
 }
 
 impl std::fmt::Debug for HuffmanTree {
@@ -52,31 +50,31 @@ impl std::fmt::Debug for HuffmanTree {
 impl HuffmanTree {
     pub fn freq(&self) -> u64 {
         match self {
-            HuffmanTree::Node{freq, ..} => *freq,
-            HuffmanTree::Terminal{freq, ..} => *freq
+            HuffmanTree::Node { freq, .. } => *freq,
+            HuffmanTree::Terminal { freq, .. } => *freq,
         }
-    } 
+    }
 
     pub fn from_frequencies(frequencies: &HashMap<Symbol, u64>) -> HuffmanTree {
         let mut heap: BinaryHeap<HuffmanTree> = BinaryHeap::new();
-        
-        for (c, freq) in frequencies.iter(){
-            heap.push(HuffmanTree::Terminal{
+
+        for (c, freq) in frequencies.iter() {
+            heap.push(HuffmanTree::Terminal {
                 symbol: *c,
-                freq: *freq
+                freq: *freq,
             })
         }
-    
+
         while heap.len() > 1 {
             let left = heap.pop().unwrap();
             let right = heap.pop().unwrap();
-            heap.push(HuffmanTree::Node { 
-                freq: left.freq()+right.freq(), 
-                left: Box::new(left), 
-                right: Box::new(right)
+            heap.push(HuffmanTree::Node {
+                freq: left.freq() + right.freq(),
+                left: Box::new(left),
+                right: Box::new(right),
             })
         }
-        
+
         heap.pop().unwrap()
     }
 }
@@ -98,23 +96,22 @@ impl PartialEq for HuffmanTree {
 }
 impl Eq for HuffmanTree {}
 
-
 trait Merge {
     fn merge(self, other: Self) -> Self;
 }
 
-impl<L,R> Merge for HashMap<L,R> 
-where 
+impl<L, R> Merge for HashMap<L, R>
+where
     L: Hash + PartialEq + Eq + PartialOrd + Ord,
     R: Hash + PartialEq + Eq + PartialOrd + Ord,
-{   
+{
     fn merge(self, other: Self) -> Self {
         self.into_iter().chain(other).collect()
     }
 }
 
-pub struct HuffmanEncoding{
-    pub encoding: HashMap<Symbol, BitVec<u8, Lsb0>>
+pub struct HuffmanEncoding {
+    pub encoding: HashMap<Symbol, BitVec<u8, Lsb0>>,
 }
 
 impl PartialEq for HuffmanEncoding {
@@ -126,20 +123,18 @@ impl Eq for HuffmanEncoding {}
 
 fn descend(t: &HuffmanTree, seq: BitVec<u8, Lsb0>) -> HashMap<Symbol, BitVec<u8, Lsb0>> {
     match t {
-        HuffmanTree::Node {left, right, .. } => {
+        HuffmanTree::Node { left, right, .. } => {
             let mut lpath = seq.clone();
             lpath.push(false);
             let l = descend(&left, lpath);
-            
+
             let mut rpath = seq.clone();
             rpath.push(true);
             let r = descend(&right, rpath);
-            
+
             l.merge(r)
-        },
-        HuffmanTree::Terminal { symbol, .. } => {
-            return HashMap::from_iter(vec![(*symbol, seq)])
         }
+        HuffmanTree::Terminal { symbol, .. } => return HashMap::from_iter(vec![(*symbol, seq)]),
     }
 }
 
@@ -147,13 +142,13 @@ fn frequency_map(input: &Vec<u8>) -> HashMap<Symbol, u64> {
     let mut hm: HashMap<Symbol, u64> = HashMap::new();
     hm.insert(Symbol::EOT, 1);
 
-    for &b in input{
+    for &b in input {
         *hm.entry(Symbol::Char(b)).or_insert(0) += 1;
     }
     hm
 }
 
-impl HuffmanEncoding{
+impl HuffmanEncoding {
     pub fn from_data_vec(input: &Vec<u8>) -> HuffmanEncoding {
         let frequencies = frequency_map(input);
         HuffmanEncoding::from_frequencies(frequencies)
@@ -165,8 +160,8 @@ impl HuffmanEncoding{
     }
 
     pub fn from_tree(tree: &HuffmanTree) -> HuffmanEncoding {
-        HuffmanEncoding{
-            encoding: descend(tree, BitVec::new())
+        HuffmanEncoding {
+            encoding: descend(tree, BitVec::new()),
         }
     }
 
@@ -183,7 +178,7 @@ impl HuffmanEncoding{
         out.append(&mut eot_bytes);
 
         for (ref c, r) in self.encoding.iter() {
-            match c{
+            match c {
                 Symbol::Char(c) => {
                     let len = r.len();
                     if len > std::u8::MAX.into() {
@@ -192,125 +187,116 @@ impl HuffmanEncoding{
                     let mut nr = r.clone();
                     nr.set_uninitialized(false);
                     let bits = nr.into_vec();
-                    
+
                     // { symbol: Symbol, len: u8, bits: [u8, len]}
                     out.push(*c);
                     out.push(len as u8);
                     out.append(&mut bits.clone());
-                },
-                Symbol::EOT => continue
+                }
+                Symbol::EOT => continue,
             }
-            
-            
         }
+        eprintln!("{:?}", out);
         return out;
     }
 
     pub fn restore_from(d: &Vec<u8>) -> HuffmanEncoding {
-        let mut encoding: HashMap<Symbol, BitVec<u8,Lsb0>> = HashMap::new(); 
+        eprintln!("{:?}", d);
+        let mut encoding: HashMap<Symbol, BitVec<u8, Lsb0>> = HashMap::new();
 
         let mut index: usize = 0;
 
         // First decode EOT
         let nbits_eot = *d.get(index).expect("Error deserialising input file");
         let nbytes_eot = (nbits_eot as f64 / 8.0).ceil() as usize;
-        let bits_eot = &d[index+1..index+nbytes_eot+1];
+        let bits_eot = &d[index + 1..index + nbytes_eot + 1];
         let mut eot_bits: BitVec<u8, Lsb0> = BitVec::from_slice(bits_eot);
         eot_bits.truncate(nbits_eot as usize);
         encoding.insert(Symbol::EOT, eot_bits);
 
-        index += nbytes_eot +1;
+        index += nbytes_eot + 1;
 
         // Decode rest of symbols
         loop {
             let c = *d.get(index).expect("Error deserialising input file");
-            let len = *d.get(index+1).expect("Error deserialising input file");
+            let len = *d.get(index + 1).expect("Error deserialising input file");
             let size = (len as f64 / 8.0).ceil() as usize;
-            let bits: &[u8] = &d[index+2..index+size+2];
+            let bits: &[u8] = &d[index + 2..index + size + 2];
             let mut v: BitVec<u8, Lsb0> = BitVec::from_slice(bits);
             v.truncate(len as usize);
-            
+
             v.set_uninitialized(false);
             encoding.insert(Symbol::Char(c), v);
 
-            index += size+2;
+            index += size + 2;
 
-            if index > d.len()-3 { break }
-        }     
+            if index > d.len() - 3 {
+                break;
+            }
+        }
 
-        return HuffmanEncoding{
-            encoding: encoding
-        };
+        return HuffmanEncoding { encoding: encoding };
     }
 
-    pub fn encode(&self,input: &Vec<u8>) -> Vec<u8> {
+    pub fn encode(&self, input: &Vec<u8>) -> Vec<u8> {
         let mut filestream = bitvec![u8, Lsb0;];
-        
-        for c in input{
+
+        for c in input {
             let mut code = self.encoding.get(&Symbol::Char(*c)).unwrap().clone();
             filestream.append(&mut code);
         }
         let mut eot = self.encoding.get(&Symbol::EOT).unwrap().clone();
         filestream.append(&mut eot);
-       
-       filestream.into_vec()
+
+        filestream.into_vec()
     }
 
     pub fn decode(self, input: &Vec<u8>) -> Vec<u8> {
         let in_bits: BitVec<u8, Lsb0> = BitVec::from_slice(input);
-        let mut out :Vec<u8> = vec![];
+        let mut out: Vec<u8> = vec![];
         let mut cursor = 0;
 
         assert!(self.encoding.contains_key(&Symbol::EOT));
 
         while cursor < in_bits.len() {
-            for ( s, pat) in self.encoding.iter(){
+            for (s, pat) in self.encoding.iter() {
                 let len = pat.len();
                 if cursor + len <= in_bits.len() {
-                    let stream = &in_bits[cursor..cursor+len];
+                    let stream = &in_bits[cursor..cursor + len];
                     if pat == stream {
                         match s {
                             Symbol::Char(c) => {
                                 out.push(*c);
                                 cursor += len;
-                                break
-                            },
-                            Symbol::EOT =>{
-                                return out
-                            } 
+                                break;
+                            }
+                            Symbol::EOT => return out,
                         }
-                        
                     }
-                }    
+                }
             }
         }
         unreachable!()
-        
     }
 
     #[allow(dead_code)]
-    pub fn diff(&self, other: &Self) -> Vec<(Symbol, BitVec<u8,Lsb0>, BitVec<u8,Lsb0>)> {
+    pub fn diff(&self, other: &Self) -> Vec<(Symbol, BitVec<u8, Lsb0>, BitVec<u8, Lsb0>)> {
         let mut diffs = Vec::new();
-        for (l,r) in self.encoding.iter(){
+        for (l, r) in self.encoding.iter() {
             let sr = self.encoding.get(l).unwrap();
             let or = other.encoding.get(l).unwrap();
-            if  sr != or {
-                diffs.push((
-                    *l,
-                    r.clone(),
-                    or.clone()
-                ))
+            if sr != or {
+                diffs.push((*l, r.clone(), or.clone()))
             }
         }
         return diffs;
     }
 }
 
-
 // impl Serialize for HuffmanEncoding {
 //     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
 //     where
-//         S: serde::Serializer 
+//         S: serde::Serializer
 //     {
 //         todo!()
 //     }
@@ -319,7 +305,7 @@ impl HuffmanEncoding{
 // impl<'de> Deserialize<'de> for HuffmanEncoding {
 //     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
 //     where
-//         D: serde::Deserializer<'de> 
+//         D: serde::Deserializer<'de>
 //     {
 //         todo!()
 //     }
@@ -328,7 +314,7 @@ impl HuffmanEncoding{
 impl std::fmt::Debug for HuffmanEncoding {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "(")?;
-        for (c, v) in self.encoding.iter(){
+        for (c, v) in self.encoding.iter() {
             write!(f, "[{:02X?}]  {}\n", *c, v)?
         }
         write!(f, ")")
--- press/src/main.rs
+++ press/src/main.rs
@@ -1,13 +1,11 @@
-
-use clap::{Arg, ArgAction};
 use clap::Command;
+use clap::{Arg, ArgAction};
 use huffman::HuffmanEncoding;
 
 use std::fs::File;
-use std::io::{Read, Write, BufReader, BufWriter, BufRead};
+use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 
 mod huffman;
-
 
 const MAGIC: [u8; 5] = [b'P', b'R', b'E', b'S', b'S'];
 
@@ -15,22 +13,29 @@ fn compress(input: &Vec<u8>, output: &mut Vec<u8>) {
     let encoding = HuffmanEncoding::from_data_vec(input);
     let mut filestream = encoding.encode(input);
     let mut code_table = encoding.save();
-    let mut tlen = u32_to_u8s(code_table.len() as u32);
-    
+
+    let mut table_len = u32_to_u8s(code_table.len() as u32);
+
     output.append(&mut Vec::from(MAGIC));
-    output.append(&mut tlen);
+    output.append(&mut table_len);
     output.append(&mut code_table);
-    output.append(&mut filestream);   
+    output.append(&mut filestream);
 }
 
 fn decompress(input: &Vec<u8>, output: &mut Vec<u8>) {
-    if input[0..MAGIC.len()] != MAGIC {
+    let magic_len = MAGIC.len();
+    if &input[0..magic_len] != &MAGIC {
         panic!("File is not in press format.");
     }
 
-    let table_length = u8s_to_u32(&Vec::from(&input[MAGIC.len()..MAGIC.len()+4])) as usize;
-    let table_raw = &input[MAGIC.len()+4..table_length+MAGIC.len()+4];
-    let data_raw = &input[table_length+MAGIC.len()+4..];
+    let u32_len: usize = 4; // 4 bytes in a u32
+    let table_length = u8s_to_u32(&Vec::from(&input[magic_len..magic_len + u32_len])) as usize;
+
+    let table_start = magic_len + u32_len;
+    let table_end = table_start + table_length;
+    let table_raw = &input[table_start..table_end];
+    let data_start = table_end;
+    let data_raw = &input[data_start..];
 
     let encoding = HuffmanEncoding::restore_from(&Vec::from(table_raw));
 
@@ -39,23 +44,25 @@ fn decompress(input: &Vec<u8>, output: &mut Vec<u8>) {
     output.append(&mut data);
 }
 
-fn main() -> Result<(), std::io::Error>  {
+fn main() -> Result<(), std::io::Error> {
     let matches = Command::new("Press")
         .version("1.0")
         .author("Brice Fernandes <brice@fractallambda.com>")
         .about("Basic file compressor.")
-        .arg(Arg::new("INPUT")
-            .help("Input file"))
-        .arg(Arg::new("OUTPUT")
-            .help("Output file."))
-        .arg(Arg::new("decompress")
-            .short('d')
-            .long("Decompress input file instead of compressing.")
-            .action(ArgAction::SetTrue))
-        .arg(Arg::new("compress")
-            .short('c')
-            .long("Compress input file to output file.")
-            .action(ArgAction::SetTrue))
+        .arg(Arg::new("INPUT").help("Input file"))
+        .arg(Arg::new("OUTPUT").help("Output file."))
+        .arg(
+            Arg::new("decompress")
+                .short('d')
+                .long("Decompress input file instead of compressing.")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("compress")
+                .short('c')
+                .long("Compress input file to output file.")
+                .action(ArgAction::SetTrue),
+        )
         .get_matches();
 
     let mut input: Box<dyn BufRead>;
@@ -96,12 +103,12 @@ fn main() -> Result<(), std::io::Error>  {
     let mut in_buf: Vec<u8> = Vec::new();
     let mut out_buf: Vec<u8> = Vec::new();
     input.read_to_end(&mut in_buf)?;
-   
+
     if matches.get_flag("decompress") {
         decompress(&in_buf, &mut out_buf);
-    }else if matches.get_flag("compress"){
+    } else if matches.get_flag("compress") {
         compress(&in_buf, &mut out_buf);
-    }else{
+    } else {
         panic!("Must specify either compression or decompression. See --help option.");
     }
 
@@ -112,22 +119,23 @@ fn main() -> Result<(), std::io::Error>  {
     return Ok(());
 }
 
-
 fn u32_to_u8s(i: u32) -> Vec<u8> {
+    // return i.to_be_bytes().to_vec();
     Vec::from([
-        (i & 0xff000000 >> 24) as u8,
-        (i & 0x00ff0000 >> 16) as u8,
-        (i & 0x0000ff00 >> 8) as u8,
-        (i & 0x000000ff) as u8,
-    ]) 
+        ((i >> 24) & 0xff) as u8,
+        ((i >> 16) & 0xff) as u8,
+        ((i >> 8) & 0xff) as u8,
+        (i & 0xff) as u8,
+    ])
 }
 
 fn u8s_to_u32(us: &Vec<u8>) -> u32 {
-    let mut out:u32 = 0;
-    out |= (us[0] as u32) & 0x000000ff << 24;
-    out |= (us[1] as u32) & 0x000000ff << 16;
-    out |= (us[2] as u32) & 0x000000ff << 8;
-    out |= us[3] as u32 & 0x000000ff;
+    // return u32::from_be_bytes(us);
+    let mut out: u32 = 0;
+    out |= (us[0] as u32) << 24;
+    out |= (us[1] as u32) << 16;
+    out |= (us[2] as u32) << 8;
+    out |= us[3] as u32;
     return out;
 }
 
@@ -140,24 +148,23 @@ mod tests {
         if input[0..MAGIC.len()] != MAGIC {
             panic!("File is not in press format.");
         }
-        u8s_to_u32(&Vec::from(&input[3..7]))
+        u8s_to_u32(&Vec::from(&input[MAGIC.len()..MAGIC.len() + 4]))
     }
 
     #[test]
-    fn can_encode_decode_u32(){
-        for i in 0..256 {
-            let xs =u32_to_u8s(i);
+    fn can_encode_decode_u32() {
+        for i in 0..400 {
+            let xs = u32_to_u8s(i);
             let y = u8s_to_u32(&xs);
-            assert_eq!(i,y);
+            assert_eq!(i, y);
         }
-        
     }
 
     #[test]
     fn can_get_encoding_table_length() {
         let in_buf: Vec<u8> = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi condimentum gravida libero non mollis. Mauris turpis sapien, interdum non tortor id, sollicitudin lobortis mi. Nam sit amet tellus vehicula, condimentum.".to_vec();
         let mut out_buf: Vec<u8> = Vec::new();
-        
+
         let encoding_original = HuffmanEncoding::from_data_vec(&in_buf);
         let saved = encoding_original.save();
         let expected_length = saved.len() as u32;
@@ -168,7 +175,7 @@ mod tests {
     }
 
     #[test]
-    fn encoding_will_always_have_eot(){
+    fn encoding_will_always_have_eot() {
         let in_buf: Vec<u8> = b"".to_vec();
         let encoding = HuffmanEncoding::from_data_vec(&in_buf);
 
@@ -176,9 +183,9 @@ mod tests {
     }
 
     #[test]
-    fn decoded_table_has_eot(){
+    fn decoded_table_has_eot() {
         let in_buf: Vec<u8> = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi condimentum gravida libero non mollis. Mauris turpis sapien, interdum non tortor id, sollicitudin lobortis mi. Nam sit amet tellus vehicula, condimentum.".to_vec();
-        
+
         let encoding = HuffmanEncoding::from_data_vec(&in_buf);
         let saved = encoding.save();
         let restored = HuffmanEncoding::restore_from(&saved);
@@ -187,24 +194,22 @@ mod tests {
     }
 
     #[test]
-    fn can_compress_decompress(){
-
+    fn can_compress_decompress() {
         let in_buf: Vec<u8> = b"Hello World".to_vec();
         let mut compressed_buf: Vec<u8> = Vec::new();
         let mut out_buf: Vec<u8> = Vec::new();
-        
+
         compress(&in_buf, &mut compressed_buf);
         println!("COMPRESSED...");
         decompress(&compressed_buf, &mut out_buf);
 
         assert_eq!(in_buf, out_buf);
-
     }
 
     #[test]
     fn test_encoding_serialise_deserialise() {
         let in_buf: Vec<u8> = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi condimentum gravida libero non mollis. Mauris turpis sapien, interdum non tortor id, sollicitudin lobortis mi. Nam sit amet tellus vehicula, condimentum.".to_vec();
-        
+
         let encoding = HuffmanEncoding::from_data_vec(&in_buf);
         let saved = encoding.save();
         let restored = HuffmanEncoding::restore_from(&saved);
